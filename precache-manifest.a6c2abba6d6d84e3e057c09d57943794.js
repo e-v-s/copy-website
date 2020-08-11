f7bcc97918b3fd1e407abc9b1d1f8803e5ddcb81
Évora evs Leite <evoraleite@gmail.com>
2020-08-11 08:49:34 -0300

    Updates

---
+++ precache-manifest.a6c2abba6d6d84e3e057c09d57943794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7955447e87b1452c56d008c0d5c6cec1",
+    "url": "/copy-website/index.html"
+  },
+  {
+    "revision": "09191436d65ea24a1c2b",
+    "url": "/copy-website/static/css/main.926dcfab.chunk.css"
+  },
+  {
+    "revision": "6a6b77687148398f4885",
+    "url": "/copy-website/static/js/2.21fe0dfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/copy-website/static/js/2.21fe0dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09191436d65ea24a1c2b",
+    "url": "/copy-website/static/js/main.2fc45064.chunk.js"
+  },
+  {
+    "revision": "1afa7d3155e56f070e9a",
+    "url": "/copy-website/static/js/runtime-main.a3c16425.js"
+  }
+]);
